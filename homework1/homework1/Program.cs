82e8ecb4c9b0247c5a24dc71f823925680932bac
fanronaldo777161 <siharylidze091007@gmail.com>
2026-05-19 22:14:24 +0300

    task1

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace homework1
+{
+    internal class Program
+    {
+        //обнуляет разряд десятков
+        static int secondnumtozero(int n)
+        {
+            int h = (n / 100) * 100;
+            int u = n % 10;
+            return h + u;
+        }
+        static void Main(string[] args)
+        {
+            //task1
+            Console.WriteLine(secondnumtozero(123));
+            Console.WriteLine(secondnumtozero(987));
+            
+        }
+    }
+}
